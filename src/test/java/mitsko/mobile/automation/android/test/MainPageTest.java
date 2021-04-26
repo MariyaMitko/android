29b68fef0db8867ed49440d69a68b512f4d07b64
mariy <mariyamitko@mail.ru>
2021-04-26 18:34:58 +0300

    update test classes

--- src/test/java/mitsko/mobile/automation/android/test/MainPageTest.java
+++ src/test/java/mitsko/mobile/automation/android/test/MainPageTest.java
@@ -1,48 +1,59 @@
 package mitsko.mobile.automation.android.test;
 
 import java.net.MalformedURLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import io.qameta.allure.Step;
 import mitsko.mobile.automation.android.pages.MainPage;
+import mitsko.mobile.automation.android.steps.Helper;
 import mitsko.mobile.automation.android.steps.Steps;
 
 public class MainPageTest {
+	
 	private Steps st;
 	private MainPage mp;
+	private Helper hp;
+	private static final Logger logger = LogManager.getRootLogger();
     
     @BeforeClass
     public void setUp() throws MalformedURLException {
 
        st = new Steps();
+       hp = new Helper();
        st.initDriver();
        mp = st.getMainPage();
+       logger.info("== WHEN app's Main page is opened ==");
     }
     
     @Test
-    public void verifyTitle() throws InterruptedException {
-
-       String title = mp.readTitle();
-                
-       Assert.assertEquals(title, "Сборник простых английских текстов", "App titple should be correct");
+    @Step("Verify main page contains correct title")
+    public void verifyTitle() {
+    	
+       logger.info("== THEN page contains correct title ==");
+       Assert.assertEquals(mp.readTitle(), hp.getExpectedTitle());
    }
          
     @Test
-    public void verifyPlayButton() throws InterruptedException {
+    @Step("Verify Play button is enabled")
+    public void verifyPlayButton() {
 
-       String title = mp.readTitle();
-       boolean buttonState = mp.isPlayBtnEnabled();
-                
-       Assert.assertEquals(buttonState, true, "Play button should be enabled");
+       logger.info("== THEN Play button is enabled ==");
+       Assert.assertEquals(mp.isPlayBtnEnabled(), true);
    }
     
     @Test
+    @Step("Verify main page contains 6 available widgets")
     public void verifyWidgets() {
 
-       String title = mp.readTitle();
        String[] actualArr = mp.getAvailableWidgets();
-       String[] excpetedArr = {"Мои книги", "Словарь", "Настройки", "Магазин", "Статистика", "Закладки"};
+       String[] excpetedArr = hp.getExpectedWidgets();
 
-       Assert.assertEquals(excpetedArr, actualArr, "App should contains available widgets");
+       logger.info("== THEN page contains 6 available widgets ==");
+       Assert.assertEquals(excpetedArr, actualArr);
    }
     
      
