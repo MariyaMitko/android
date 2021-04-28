140df99a8cc05c8d3d47a1440cf7c91340de6c55
mariy <mariyamitko@mail.ru>
2021-04-28 11:38:07 +0300

    code refactoring

--- src/test/java/mitsko/mobile/automation/android/test/WordsPageTest.java
+++ src/test/java/mitsko/mobile/automation/android/test/WordsPageTest.java
@@ -1,68 +1,79 @@
 package mitsko.mobile.automation.android.test;
 
 import java.net.MalformedURLException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import io.qameta.allure.Step;
 import mitsko.mobile.automation.android.objects.TranslatedWord;
 import mitsko.mobile.automation.android.pages.MainPage;
 import mitsko.mobile.automation.android.pages.WordsPage;
 import mitsko.mobile.automation.android.steps.Steps;
 
 public class WordsPageTest {
+	
 	private Steps st;
 	private MainPage mp;
 	private WordsPage wp;
+	private static final Logger logger = LogManager.getRootLogger();
     
 	@BeforeClass
     public void setUp() throws MalformedURLException {
     	
-    	  st = new Steps();
-          st.initDriver();
-       	  mp = st.getMainPage();
-          mp.openSavedWords();
-          wp = st.getWordsPage();
+    	st = new Steps();
+        st.initDriver();
+       	mp = st.getMainPage();
+        mp.openSavedWords();
+        wp = st.getWordsPage();
+        logger.info("== WHEN user open Saved Words page ==");
     }
                
     @Test
+    @Step("Verify 'Add new' button")
     public void verifyAddButton() {
 
-        boolean buttonState = wp.isAddWordBtnEnabled();
-                
-        Assert.assertEquals(buttonState, true, "Add new word button should be enabled");
+        logger.info("== THEN button 'Add new' is enabled ==");        
+        Assert.assertEquals(wp.isAddWordBtnEnabled(), true);
     }
     
     @Test(dependsOnMethods={"verifyAddButton"})
+    @Step("Verify translation modal")
     public void verifyAddTranslationModal() {
     	
+    	logger.info("== WHEN user click 'Add new' ==");
     	wp.openTranslationModal();
 
-        boolean modalPresense = wp.isTranslateWordModalDisplayed();
-                
-        Assert.assertEquals(modalPresense, true, "Add translation modal shpild be opened after click on Add btn");
+    	logger.info("== THEN translation modal is opened ==");          
+        Assert.assertEquals(wp.isTranslateWordModalDisplayed(), true);
     }
     
     @Test(dependsOnMethods={"verifyAddTranslationModal"})
+    @Step("Verify translated and saved word")
     public void verifyTranslation() {
     	
+    	logger.info("== WHEN user add word translation ==");
     	TranslatedWord word = new TranslatedWord("Кот", "cat");
+    	
     	wp.addTranslation(word.getWord(), word.getTranslation());
-
     	TranslatedWord[] actualArr = wp.getSavedWords();
-    	int size = actualArr.length;
-                
-        Assert.assertEquals(size, 1, "Word should be added");
+    	
+    	logger.info("== THEN translation is saved ==");         
+        Assert.assertEquals(actualArr.length, 1);
         word.equals(actualArr[0]);
     }
     
     @Test(dependsOnMethods={"verifyTranslation"})
+    @Step("Verify user can remove saved word")
     public void verifyWordRemoving() {
     	
+    	logger.info("== WHEN user click word row ==");
+    	logger.info("== AND select 'Remove' ==");
       	wp.deleteWord();
       	      	
-      	boolean wordPresense = wp.isWordRowPresented();
-         
-        Assert.assertEquals(wordPresense, false, "Word shoild be deleted");
+      	logger.info("== THEN word is removed ==");   
+        Assert.assertEquals(wp.isWordRowPresented(), false);
       
     }
         
